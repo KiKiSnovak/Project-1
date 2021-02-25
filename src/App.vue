ce9746627de288918766665f57fea06366e779a8
Kristian Snovak <44963752+KiKiSnovak@users.noreply.github.com>
2021-02-25 16:48:46 +0100

    vue app

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
